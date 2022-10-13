5229d9a9def35915d63b32bdcc390deb24126c93
ryl <renyouliang69@163.com>
2022-10-13 20:46:54 +0800

    增量业务数据首日导入脚本

---
+++ shell/mysql_to_hdfs_inc_init.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "参数能为空! 请输入要同步的增量表名，同步所有增量表请输入 all "
+fi
+
+MAXWELL_HOME=/opt/module/maxwell
+
+import_data() {
+  $MAXWELL_HOME/bin/maxwell-bootstrap --database gmall --table $1 --config $MAXWELL_HOME/config.properties
+}
+
+case $1 in
+"cart_info")
+  import_data cart_info
+;;
+"coupon_use")
+  import_data coupon_use
+;;
+"comment_info")
+  import_data comment_info
+;;
+"favor_info")
+  import_data favor_info
+;;
+"order_detail")
+  import_data order_detail
+;;
+"order_detail_activity")
+  import_data order_detail_activity
+;;
+"order_detail_coupon")
+  import_data order_detail_coupon
+;;
+"order_info")
+  import_data order_info
+;;
+"order_refund_info")
+  import_data order_refund_info
+;;
+"order_status_log")
+  import_data order_status_log
+;;
+"payment_info")
+  import_data payment_info
+;;
+"refund_payment")
+  import_data refund_payment
+;;
+"user_info")
+  import_data user_info
+;;
+"all")
+import_data cart_info
+import_data coupon_use
+import_data comment_info
+import_data favor_info
+import_data order_detail
+import_data order_detail_activity
+import_data order_detail_coupon
+import_data order_info
+import_data order_refund_info
+import_data order_status_log
+import_data payment_info
+import_data refund_payment
+import_data user_info
+;;
+esac
